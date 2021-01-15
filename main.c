3f8dfbbfd139c7232bc164479384acde3271ecfd
Miguelangel17 <75395035+alvareztuamiguelangel@users.noreply.github.com>
2021-01-15 07:23:59 -0500

    v6

--- main.c
+++ main.c
@@ -1,23 +1,32 @@
-//DESAROLLO DE UN ROL DE PAGOS 
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct Empresa{
-	char Nombre[20];
-	char RUC[15];
-	char Direccion[30];
-	char Fecha[30];
-}Empresa;
-int main(){
-	struct Empresa i;
-	int d;
-	printf("Datos Empresa\n");
-	printf("NOMBRE:\t");
-	scanf(" %s",&i.Nombre);
-	printf("RUC:\t");
-	scanf(" %s",&i.RUC);
-	printf("DIRECION:\t");
-	scanf(" %s",&i.Direccion);
-	printf("FECHA:\t");
-	scanf(" %s",&i.Fecha);
-return 0;
+typedef struct Empleados{
+	char Nombre[10];
+	char Apellido[10];
+	char Cargo[10];
+	char SueldoBasico[10];
+	int Horasextras50;
+	int Horasextras100;
+	
+} Empleados;
+int main(int argc, char *argv[]) {
+	struct Empleados e[10]; 
+	int i = 0;
+	for(i = 0; i< 10;i++){
+		printf("Ingrese los datos de los Empleados %d\n",(i+1));
+		printf("Nombre: \t");
+		scanf("%s",&e[i].Nombre); 
+		printf("Apellido: \t");
+		scanf("%s",&e[i].Apellido);
+		printf("Cargo: \t");
+		scanf("%s",&e[i].Cargo); 	
+		printf("SueldoBasico: \t");
+		scanf("%s",&e[i].SueldoBasico); 
+		printf("Horasextras50: \t");
+		scanf("%s",&e[i].Horasextras50);
+	    printf("Horasextras100: \t");
+		scanf("%s",&e[i].Horasextras100);
+		 
+}
+	return 0;
 }
